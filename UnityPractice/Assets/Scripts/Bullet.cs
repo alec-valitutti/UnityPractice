85b6819ad65f9a437a8ffc52b79b9675fc1f4b1d
alec-valitutti <alecvalitutti@gmail.com>
2021-12-06 21:41:22 -0500

    we pushin

---
+++ UnityPractice/Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public float counter;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(new Vector3(10,10,10)*Time.deltaTime);
+        transform.Translate(transform.forward * speed *Time.deltaTime);
+        counter -= 5 * Time.deltaTime;
+        if (counter <=0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Wall")
+        {
+            Destroy(this.gameObject);
+        }
+        if (other.tag == "Breakable")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ UnityPractice/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public CharacterController c;
+    public GameObject Bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*I don't think this is the most optimized, assigning this in the 
+         * inspector is the way to do it but idc...
+        */
+        //c = this.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+        PlayerShoot();
+    }
+    public void PlayerMove()
+    {
+        #region You could use this code to move an object around with the keyboard but I wouldn't reccomend it
+        var move = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        c.Move(move * Time.deltaTime * Speed);
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    this.gameObject.transform.Translate(new Vector3(0,0,Speed*Time.deltaTime));
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    this.gameObject.transform.Translate(new Vector3(0, 0, -Speed * Time.deltaTime));
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    this.gameObject.transform.Translate(new Vector3(-Speed * Time.deltaTime, 0, 0));
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    this.gameObject.transform.Translate(new Vector3(Speed * Time.deltaTime, 0, 0));
+        //}
+        #endregion
+        // Using CharacterController component
+
+    }
+    public void PlayerShoot()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //instatiate the bullet object
+
+            Instantiate(Bullet,
+                new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z + 1)
+                ,this.transform.rotation);
+        }
+    }
+
+}
